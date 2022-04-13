b5bcaf03f3c1d6c8b22fdf36371ebb8801eb5027
Benjamin <benjaminrowe0@gmail.com>
2022-04-14 08:08:55 +1000

    commit tree e31f19362d6616fd8c116fa4c2970450c6575dd5

--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,12 @@
-def using_while
+def using_while()
   levitation_force = 6
-	
-  #your code here
+	#your code here
+  while levitation_force <= 9 do
+    puts "Wingardium Leviosa"
+    levitation_force = levitation_force + 1 
+  end
+
 end
 
 
+using_while()
